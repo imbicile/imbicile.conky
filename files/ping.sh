6311c1d3a04b3e09bb0e2d5b40be856545a6bdf2
Stanislav Kononov <35522330+imbicile@users.noreply.github.com>
2026-06-10 08:53:06 +0300

    replace ping script

--- files/ping.sh
+++ files/ping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if ping -c 1 -w 1 8.8.8.8 &>/dev/null; then
-    curl https://ifconfig.me/
+if ping -c 1 -w 1 ipinfo.io &>/dev/null; then
+    curl -ks https://ipinfo.io | jq -r '.ip'
 else
     echo offline
 fi
